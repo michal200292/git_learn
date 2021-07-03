cfe4e0b28117b8c9ccf94fe6758a2eaf7421e35d
FilipNozkiewicz <filas08@gmail.com>
2021-07-03 13:55:37 +0200

    sth changed

--- hello.py
+++ hello.py
@@ -5,3 +5,20 @@ for i in range(10):
 print("Tak")
 for i in range(10):
     print(i, end=" ")
+
+
+
+def my_dec(f):
+    def wrapper():
+        print("Follow the white rabbit")
+        f()
+    return wrapper
+
+@my_dec
+def ff():
+    try:
+        print("Hello from Filip")
+    except:
+        pass
+
+ff()
